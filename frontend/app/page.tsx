711f68bcca8b8af9cac179d9a2595feea6ce2021
Shreyash Gupta <48386323+shreyashkgupta@users.noreply.github.com>
2025-05-24 01:07:58 +0530

    Basic project setup

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-screen gap-4">
+      <h1 className="text-4xl font-bold">Basic Setup</h1>
+      <Button>Shadcn UI</Button>
+    </div>
+  );
+}
